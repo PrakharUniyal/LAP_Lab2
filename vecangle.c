ca79908948c387e6bf69b483a93c7951269f574e
Prakhar Uniyal <b18128@students.iitmandi.ac.in>
2020-10-15 20:20:21 +0530

    Fixed the vectorangle function

--- vecangle.c
+++ vecangle.c
@@ -12,6 +12,6 @@ float vectorangle(vector vec_a, vector vec_b)
     float norm_a = vectornorm(vec_a,vector_zero);
     float norm_b = vectornorm(vec_b, vector_zero);
     
-    theta = dotprod/(norm_a,norm_b);
+    theta = acos(dotprod/(norm_a*norm_b));
     return theta;
 }
